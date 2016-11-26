69580a1ca86d3123b9e4ced2f29d48fc2378a065
osem <oksana.semenkova@yahoo.com>
2016-11-26 23:09:26 +0200

    Simple REST client was added

---
+++ src/RestClient.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by osemenkova on 11/26/2016.
+ */
+public class RestClient {
+    public static void main(String []args){
+        try {
+
+            double a = 2;
+            double b = 8;
+            String operation = "sum";
+
+            URL url = new URL("http://localhost:8080/SimpleRestServer?a=" + a + "&b=" + b + "&operation=" + operation);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+    }
+}
