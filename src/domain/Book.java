866698597e21f8d02ee983c7d5d174dfb89fcdfc
osem <oksana.semenkova@yahoo.com>
2016-11-27 10:45:31 +0200

    REST server with Resteasy was implemented

---
+++ src/domain/Book.java
@@ -0,0 +1,64 @@
+package domain;
+
+/**
+ * Created by osemenkova on 11/27/2016.
+ */
+public class Book {
+    private int id;
+    private String author;
+    private String title;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        if (id != book.id) return false;
+        if (author != null ? !author.equals(book.author) : book.author != null) return false;
+        return title != null ? title.equals(book.title) : book.title == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/rest/BookServiceResteasy.java
@@ -0,0 +1,37 @@
+package rest;
+
+import domain.Book;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by osemenkova on 11/27/2016.
+ */
+
+@Path("/rest")
+public class BookServiceResteasy {
+
+    @GET
+    @Path("/get")
+    @Produces("application/json")
+    public Book getBookInJSON() {
+
+        Book book = new Book();
+        book.setTitle("Java");
+        book.setAuthor("Ekkel");
+        book.setId(1);
+
+        return book;
+    }
+
+    @POST
+    @Path("/create")
+    @Consumes("application/json")
+    public Response createBookInJSON(Book book) {
+
+        String result = "Book created : " + book;
+        return Response.status(201).entity(result).build();
+
+    }
+}
