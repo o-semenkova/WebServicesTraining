a54b9851753a4fd9bfcf05379660f683eb45c881
osem <oksana.semenkova@yahoo.com>
2016-11-27 11:08:27 +0200

    REST client with Resteasy was implemented

---
+++ src/rest/BookClientResteasy.java
@@ -0,0 +1,52 @@
+package rest;
+
+import com.google.gson.Gson;
+import org.apache.http.client.ClientProtocolException;
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
+import domain.Book;
+
+import java.io.IOException;
+
+/**
+ * Created by osemenkova on 11/27/2016.
+ */
+public class BookClientResteasy {
+
+    public static void main(String[] args) {
+
+        try {
+            ClientRequest request = new ClientRequest(
+                    "http://localhost:8080/rest/get");
+            request.accept("application/json");
+            ClientResponse<String> response = request.get(String.class);
+
+            if (response.getStatus() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + response.getStatus());
+            }
+
+            Gson gson = new Gson();
+            Book book = gson.fromJson(response.getEntity(), Book.class);
+
+            System.out.println("Output from Server .... \n");
+
+            System.out.println(book.toString());
+
+
+        } catch (ClientProtocolException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+}
