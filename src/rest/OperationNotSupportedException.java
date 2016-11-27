af6e365c0db20467e4d36bba5e3d39602b2a9f5e
osem <oksana.semenkova@yahoo.com>
2016-11-27 12:37:02 +0200

    REST server with Jersey: refactoring

---
+++ src/rest/OperationNotSupportedException.java
@@ -0,0 +1,7 @@
+package rest;
+
+/**
+ * Created by osemenkova on 11/27/2016.
+ */
+public class OperationNotSupportedException extends Exception {
+}
--- src/rest/RestServer.java
+++ src/rest/RestServer.java
@@ -16,46 +16,39 @@ public class RestServer extends HttpServlet {
 
 @GET
 @Produces(MediaType.APPLICATION_JSON)
-public Response calculateWithGet(@QueryParam("a") double a, @QueryParam("b") double b, @QueryParam("operation") String operation) {
-
-    double calculationResult = 0;
-
-    if(operation.equals("sum")){
-        calculationResult = a + b;
-    }else if(operation.equals("multiply")){
-        calculationResult = a * b;
-    }else if(operation.equals("divide")){
-        calculationResult = a / b;
-    }else if(operation.equals("subtract")){
-        calculationResult = a - b;
-    }
+public Response calculateWithGet(@QueryParam("a") double a, @QueryParam("b") double b, @QueryParam("operation") String operation) throws OperationNotSupportedException {
 
     Gson gson = new Gson();
-    String output = gson.toJson(calculationResult);
+    String output = gson.toJson(calculate(a, b, operation));
 
     return Response.status(200).entity(output).build();
 
 }
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public Response calculateWithPost(@QueryParam("a") double a, @QueryParam("b") double b, @QueryParam("operation") String operation) {
-
-        double calculationResult = 0;
-
-        if(operation.equals("sum")){
-            calculationResult = a + b;
-        }else if(operation.equals("multiply")){
-            calculationResult = a * b;
-        }else if(operation.equals("divide")){
-            calculationResult = a / b;
-        }else if(operation.equals("subtract")){
-            calculationResult = a - b;
-        }
+    public Response calculateWithPost(@QueryParam("a") double a, @QueryParam("b") double b, @QueryParam("operation") String operation) throws OperationNotSupportedException {
 
         Gson gson = new Gson();
-        String output = gson.toJson(calculationResult);
+        String output = gson.toJson(calculate(a, b, operation));
 
         return Response.status(200).entity(output).build();
+    }
+
+    private double calculate(double a, double b, String operation) throws OperationNotSupportedException {
+        double calculationResult = 0;
+
+        if(operation.equalsIgnoreCase("sum")){
+            calculationResult = a + b;
+        }else if(operation.equalsIgnoreCase("multiply")){
+            calculationResult = a * b;
+        }else if(operation.equalsIgnoreCase("divide")){
+            calculationResult = a / b;
+        }else if(operation.equalsIgnoreCase("subtract")){
+            calculationResult = a - b;
+        }else{
+            throw new OperationNotSupportedException();
+        }
+        return calculationResult;
     }
 
 }
